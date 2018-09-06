905d25e65627aba343038d431a45a1073148fef7
Snarfer <Snarfer@users.noreply.github.com>
2018-09-06 09:45:01 -0400

    Arduino Buildup
    Adding experimental code to tie DOF events to Arduino.    This successfully ran a solenoid in response to flipper events.     Needs to all be dynmaic but the thrust of the idea is valid and works.

--- DirectOutput/Cab/Out/ArduinoUno/ArduinoUnoController.cs
+++ DirectOutput/Cab/Out/ArduinoUno/ArduinoUnoController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 
@@ -13,7 +14,7 @@ namespace DirectOutput.Cab.Out.NullOutputController
      /// </summary>
     public class ArduinoUnoController : OutputControllerBase, IOutputController
     {
-
+        private System.IO.Ports.SerialPort serialPort1;
 
         /// <summary>
         /// Init initializes the ouput controller.<br />
@@ -22,6 +23,34 @@ namespace DirectOutput.Cab.Out.NullOutputController
         /// <param name="Cabinet">The cabinet object which is using the output controller instance.</param>
         public override void Init(Cabinet Cabinet)
         {
+            this.serialPort1 = new System.IO.Ports.SerialPort();
+            string portNum = "";
+            string port = "";
+            serialPort1.Close();
+
+            foreach (string ports in SerialPort.GetPortNames())
+            {
+                portNum = ports.ToString();
+
+                //port = textBox1.Text;
+            }
+            port = "COM3";
+            try
+            {
+                serialPort1.PortName = (port);
+                serialPort1.BaudRate = 57600;
+                serialPort1.DataBits = 8;
+                serialPort1.Parity = Parity.None;
+                serialPort1.StopBits = StopBits.One;
+                serialPort1.Handshake = Handshake.None;
+                serialPort1.Encoding = System.Text.Encoding.Default;
+            }
+            catch
+            {
+                throw new Exception("I am too old for this shit.");
+            }
+
+
         }
 
         /// <summary>
@@ -61,11 +90,45 @@ namespace DirectOutput.Cab.Out.NullOutputController
         /// <summary>
         /// This method is called whenever the value of a output in the Outputs property changes its value.<br />
         /// This method must implement the logic to trigger the update the physical outputs.
-        /// </summary>
+        /// </summary>a
         /// <param name="Output">The output.</param>
         protected override void OnOutputValueChanged(IOutput Output)
         {
+            //outpout { Output.Name}, { Output.Value}
+            if (Output.Name == ".01" && Output.Value == 255)
+            {
+                serialPort1.Open();
+                serialPort1.WriteLine("on");
+                serialPort1.Close();
+            }
+            else if (Output.Name == ".01" && Output.Value == 0)
+            {
+                serialPort1.Open();
+                serialPort1.WriteLine("off");
+                serialPort1.Close();
+            }
+            else if (Output.Name == ".02" && Output.Value == 255)
+            {
+                serialPort1.Open();
+                serialPort1.WriteLine("on");
+                serialPort1.Close();
+            }
+            else if (Output.Name == ".02" && Output.Value == 0)
+            {
+                serialPort1.Open();
+                serialPort1.WriteLine("off");
+                serialPort1.Close();
+            }
 
+
+
+
+            //outpout ".01", 255
+            //outpout ".01", 0
+            //outpout ".02", 255
+            //outpout ".02", 0
+            //outpout ".01", 255
+            //outpout ".02", 255
         }
     }
 }
